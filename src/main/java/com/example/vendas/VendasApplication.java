c3f1c7001099c64be9abb591e27b4d8ee08ac48d
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-07-30 10:25:39 -0300

    added a helloWorld endpoint and a ping endpoint to test the application health

--- src/main/java/com/example/vendas/VendasApplication.java
+++ src/main/java/com/example/vendas/VendasApplication.java
@@ -2,9 +2,22 @@ package com.example.vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "hello World maneiro!";
+	}
+
+	@GetMapping("/ping")
+	public String ping(){
+		return "PONG";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
