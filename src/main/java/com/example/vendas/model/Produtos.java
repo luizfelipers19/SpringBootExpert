bf1b6f435511cac946be1680c258cb470c605112
Luiz Felipe R. Silveira <luiz.silveira@mercadolivre.com>
2022-08-08 21:24:19 -0300

    mapped the Produtos entity to be a JPA entity

--- src/main/java/com/example/vendas/model/Produtos.java
+++ src/main/java/com/example/vendas/model/Produtos.java
@@ -1,12 +1,24 @@
 package com.example.vendas.model;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "Produtos")
 public class Produtos {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Integer id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "descricao")
     private String descricao;
+
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
 
